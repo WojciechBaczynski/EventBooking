603bfbee5d1a53ad8dff2f90d350d203c50d1fb0
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-03-03 20:55:18 +0100

    Refactor code & bug fixes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-
 const graphqlSchema = require('./graphql/schema/index');
 const graphqlResolvers = require('./graphql/resolvers/index');
 
